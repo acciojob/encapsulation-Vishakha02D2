954fda55195ee13e1c511a71039dded67c34e8d2
Vishakha02D2 <dandagevishakha@gmail.com>
2023-01-13 00:45:12 +0530

    encapsulation solution

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+
+        public String getname() {
+
+            return name;
+
+        }
+        public void setname(String newname) {
+
+            name = newname;
+
+        }
 }
